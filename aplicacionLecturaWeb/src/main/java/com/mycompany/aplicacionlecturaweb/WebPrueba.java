5b749dfb4c3f7ea9506904ce939f6bce7ee79fc7
Laura Catalá <cuentaseria@hotmail.es>
2015-11-18 13:15:50 +0100

    Creación del proyecto

--- aplicacionLecturaWeb/src/main/java/com/mycompany/aplicacionlecturaweb/WebPrueba.java
+++ aplicacionLecturaWeb/src/main/java/com/mycompany/aplicacionlecturaweb/WebPrueba.java
@@ -5,10 +5,21 @@
  */
 package com.mycompany.aplicacionlecturaweb;
 
+import java.io.IOException;
+import java.net.URL;
+import net.htmlparser.jericho.Segment;
+import net.htmlparser.jericho.Source;
+import net.htmlparser.jericho.TextExtractor;
+
 /**
  *
  * @author formacion
  */
 public class WebPrueba {
-    
+    public static void main (String[] args) throws IOException{
+        Source web1 = new Source(new URL("http://www.google.es"));
+        Segment web2 = new Segment(web1,0,web1.length()-1);
+        TextExtractor web3 = new TextExtractor(web2);
+        System.out.println(web3.toString());
+    }
 }
